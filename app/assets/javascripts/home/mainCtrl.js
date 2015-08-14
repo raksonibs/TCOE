17bf651d1778a22f490f6bcc8ceb8f73ff5c8124
Oskar Niburski <oskarniburski@gmail.com>
2015-08-13 22:45:25 -0400

    Finding bugs

--- app/assets/javascripts/home/mainCtrl.js
+++ app/assets/javascripts/home/mainCtrl.js
@@ -4,26 +4,20 @@ angular.module('photographerNews')
 'posts',
 function($scope, posts){
 
-	resolve: {
-		postPromise: ['posts', function(posts){
-			return posts.getAll();
-		}]
-	};
+  $scope.posts = posts.posts;
 
-	$scope.posts = posts.posts;
+  $scope.addPost = function(){
+    if(!$scope.title || $scope.title === ''){ return; }
+    posts.create({
+      title: $scope.title,
+      link: $scope.link,
+    });
+    $scope.title = '';
+    $scope.link = '';
+  };
 
-	$scope.addPost = function(){
-		if(!$scope.title || $scope.title === ''){ return; }
-		posts.create({
-			title: $scope.title,
-			link: $scope.link,
-		});
-		$scope.title = '';
-		$scope.link = '';
-	};
-
-	$scope.incrementUpvotes = function(post){
-		post.upvotes += 1;
-	};
+  $scope.incrementUpvotes = function(post){
+    post.upvotes += 1;
+  };
 
 }]);
--- app/assets/javascripts/posts/postsCtrl.js
+++ app/assets/javascripts/posts/postsCtrl.js
@@ -1,20 +1,22 @@
 angular.module('photographerNews')
 .controller('PostsCtrl', [
 '$scope',
-'$stateParams',
 'posts',
-function($scope, $stateParams, posts){
-	$scope.post = posts.posts[$stateParams.id];
-	$scope.addComment = function(){
-		if($scope.body === '') { return; }
-		$scope.post.comments.push({
-			body: $scope.body,
-			author: 'user',
-			upvotes: 0
-		});
-		$scope.body = '';
-	};
-	$scope.incrementUpvotes = function(post){
-		post.upvotes += 1;
-	};
+'$stateParams',
+function($scope, posts, $stateParams){
+  $scope.post = posts.posts[$stateParams.id];
+
+  $scope.addComment = function(){
+    if($scope.body === '') { return; }
+    posts.addComment(post.id, {
+      body: $scope.body,
+      author: 'user',
+    }).success(function(comment) {
+      $scope.post.comments.push(comment);
+    });
+    $scope.body = '';
+  };
+  $scope.incrementUpvotes = function(post) {
+    posts.upvoteComment(post, comment);
+  };
 }]);
