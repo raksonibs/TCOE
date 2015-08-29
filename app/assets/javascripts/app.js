305b56332b634fb4dd43e7fd10ebf1d2cc4c6f32
Oskar Niburski <oskarniburski@gmail.com>
2015-08-29 15:43:29 -0400

    That is better. Now edit and stuff for said posts

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,36 +4,36 @@ myApp.config(['NgAdminConfigurationProvider', function (nga) {
     var admin = nga.application('My First Admin')
       .baseApiUrl('http://localhost:3000/'); // main API endpoint
 
-    var user = nga.entity('users'); // the API endpoint for users will be 'http://jsonplaceholder.typicode.com/users/:id
-        user.listView()
-        .fields([
-            nga.field('name').isDetailLink(true),
-            nga.field('username'),
-            nga.field('email')
-        ])
-    user.creationView().fields([
-        nga.field('name')
-            .validation({ required: true, minlength: 3, maxlength: 100 }),
-        nga.field('username')
-            .attributes({ placeholder: 'No space allowed, 5 chars min' })
-            .validation({ required: true, pattern: '[A-Za-z0-9\.\-_]{5,20}' }),
-        nga.field('email', 'email')
-            .validation({ required: true }),
-        nga.field('address.street')
-            .label('Street'),
-        nga.field('address.city')
-            .label('City'),
-        nga.field('address.zipcode')
-            .label('Zipcode')
-            .validation({ pattern: '[A-Z\-0-9]{5,10}' }),
-        nga.field('phone'),
-        nga.field('website')
-            .validation({ validator: function(value) {
-                if (value.indexOf('http://') !== 0) throw new Error ('Invalid url in website');
-            } })
-    ]);
-    user.editionView().fields(user.creationView().fields());
-    admin.addEntity(user)
+    // var user = nga.entity('users'); // the API endpoint for users will be 'http://jsonplaceholder.typicode.com/users/:id
+    //     user.listView()
+    //     .fields([
+    //         nga.field('name').isDetailLink(true),
+    //         nga.field('username'),
+    //         nga.field('email')
+    //     ])
+    // user.creationView().fields([
+    //     nga.field('name')
+    //         .validation({ required: true, minlength: 3, maxlength: 100 }),
+    //     nga.field('username')
+    //         .attributes({ placeholder: 'No space allowed, 5 chars min' })
+    //         .validation({ required: true, pattern: '[A-Za-z0-9\.\-_]{5,20}' }),
+    //     nga.field('email', 'email')
+    //         .validation({ required: true }),
+    //     nga.field('address.street')
+    //         .label('Street'),
+    //     nga.field('address.city')
+    //         .label('City'),
+    //     nga.field('address.zipcode')
+    //         .label('Zipcode')
+    //         .validation({ pattern: '[A-Z\-0-9]{5,10}' }),
+    //     nga.field('phone'),
+    //     nga.field('website')
+    //         .validation({ validator: function(value) {
+    //             if (value.indexOf('http://') !== 0) throw new Error ('Invalid url in website');
+    //         } })
+    // ]);
+    // user.editionView().fields(user.creationView().fields());
+    // admin.addEntity(user)
 
     var post = nga.entity('posts'); // the API endpoint for users will be 'http://jsonplaceholder.typicode.com/users/:id
     post.readOnly();
@@ -45,10 +45,6 @@ myApp.config(['NgAdminConfigurationProvider', function (nga) {
                     if (!value) return '';
                     return value.length > 50 ? value.substr(0, 50) + '...' : value;
                 }),
-            nga.field('userId', 'reference')
-                .targetEntity(user)
-                .targetField(nga.field('username'))
-                .label('Author')
         ])
         .listActions(['show'])
         .batchActions([])
@@ -57,18 +53,10 @@ myApp.config(['NgAdminConfigurationProvider', function (nga) {
                 .label('')
                 .pinned(true)
                 .template('<div class="input-group"><input type="text" ng-model="value" placeholder="Search" class="form-control"></input><span class="input-group-addon"><i class="glyphicon glyphicon-search"></i></span></div>'),
-            nga.field('userId', 'reference')
-                .targetEntity(user)
-                .targetField(nga.field('username'))
-                .label('User')
         ]);
     post.showView().fields([
         nga.field('title'),
         nga.field('body', 'text'),
-        nga.field('userId', 'reference')
-            .targetEntity(user)
-            .targetField(nga.field('username'))
-            .label('User'),
         nga.field('comments', 'referenced_list')
             .targetEntity(nga.entity('comments'))
             .targetReferenceField('postId')
@@ -82,7 +70,6 @@ myApp.config(['NgAdminConfigurationProvider', function (nga) {
     admin.addEntity(post)
 
     admin.menu(nga.menu()
-        .addChild(nga.menu(user).icon('<span class="glyphicon glyphicon-user"></span>'))
         .addChild(nga.menu(post).icon('<span class="glyphicon glyphicon-pencil"></span>'))
     );
 
