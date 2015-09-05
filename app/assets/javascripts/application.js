b11a2f125e8d80ceb35cb26efe5212413d44c274
Oskar Niburski <oskarniburski@gmail.com>
2015-09-05 12:42:18 -0400

    Start of paralax scroll

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -43,3 +43,15 @@ function navToggle(e) {
 
 
 })
+
+$(window).scroll(function(){
+  var position = $(this).scrollTop();
+  $('.imgHolder img').css({top:position/2});
+});
+
+$('.header').on('click','',function(){
+    var top = $('section').first().position().top;
+   $("html, body").animate({
+                            scrollTop: top}, {easing: "swing", duration: 800
+                           });
+});
